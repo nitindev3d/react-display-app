32677d20dd4f35a7290a2107830afc1707ec9ea4
Nitin Srivastava <nitin.dev3d@gmail.com>
2024-12-08 05:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div id="test">
+        <p>This is a paragraph inside the element with ID "test".</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
